b1c2bcad8cf755daa2cb803a9e7214ee7dcdc7a2
Aaron <aaron@noreply.midgar.city>
2026-05-13 11:13:37 +0000

    Fix error casing

--- lib/DashboardTile.ts
+++ lib/DashboardTile.ts
@@ -1,3 +1,3 @@
-export type ErrorDashboardTile = { type: 'Error'; error: unknown };
+export type ErrorDashboardTile = { type: 'error'; error: unknown };
 
 export type DashboardTile = { type: string; [key: string]: unknown };
